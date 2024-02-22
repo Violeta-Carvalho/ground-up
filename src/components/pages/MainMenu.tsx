b1ad3f19de100e40194e69744e9ad22a05076f45
Violeta Carvalho <violetadecarvalho@gmail.com>
2024-02-22 18:03:57 -0300

    Adding logo

--- src/components/pages/MainMenu.tsx
+++ src/components/pages/MainMenu.tsx
@@ -2,6 +2,7 @@ import React from "react";
 import Button from "../atoms/Button/Button";
 import { useNavigate } from "react-router-dom";
 import initialItems from "../../utils/initialItems.json";
+import logo from "./logo.svg";
 
 function MainMenu() {
   const navigator = useNavigate();
@@ -16,6 +17,9 @@ function MainMenu() {
   return (
     <div className="App">
       <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <h1>Ground Up</h1>
+        <h2>Make everything!</h2>
         <Button title="Start game" onClick={() => newGame()} />
       </header>
     </div>
